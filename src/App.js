0f407ac49766d84e82accf484bf5e5447db4225e
Dmitrii Aleksandrov <rakagrakag@gmail.com>
2022-11-01 21:39:10 +0300

    sort done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './styles/App.css';
 import PostList from "./Components/PostList";
 import PostForm from "./Components/PostForm";
+import MySelect from "./Components/UI/select/MySelect";
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         {id: 3, title: 'Javascript', body: 'Description'}
     ]);
 
+    const [selectedSort, setSelectedSort] = useState('')
+
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -21,10 +24,26 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue='Сортировка'
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'}
+                    ]}
+                />
+            </div>
             {posts.length !== 0
                 ? <PostList remove={removePost} posts={posts} title="Список постов"/>
                 : <h1 style={{textAlign: "center"}}>Посты не найдены!</h1>
