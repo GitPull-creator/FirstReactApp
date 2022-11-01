139d94712aaabeb61fdfce7af4934d8aa93672d5
Dmitrii Aleksandrov <rakagrakag@gmail.com>
2022-11-01 20:52:32 +0300

    remove done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import './styles/App.css';
 import PostList from "./Components/PostList";
-import MyButton from "./Components/UI/button/MyButton";
-import MyInput from "./Components/UI/input/MyInput";
 import PostForm from "./Components/PostForm";
 
 
@@ -15,16 +13,19 @@ function App() {
     ]);
 
 
-
     const createPost = (newPost) => {
-            setPosts([...posts, newPost])
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
 
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title="Список постов"/>
+            <PostList remove={removePost} posts={posts} title="Список постов"/>
         </div>
     );
 }
