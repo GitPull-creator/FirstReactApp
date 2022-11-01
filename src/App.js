c8a88ffacd345ad83ddc8fa375c24f0d0acdb94c
Dmitrii Aleksandrov <rakagrakag@gmail.com>
2022-11-01 20:39:59 +0300

    PostForm done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/input/MyInput";
+import PostForm from "./Components/PostForm";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript', body: 'Description'},
+        {id: 3, title: 'Javascript', body: 'Description'}
+    ]);
+
+
+
+    const createPost = (newPost) => {
+            setPosts([...posts, newPost])
+    }
+
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <PostList posts={posts} title="Список постов"/>
+        </div>
+    );
 }
 
 export default App;
